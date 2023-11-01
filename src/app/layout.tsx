dcffd8a6f7d3612accf0634a5278d5a127d20d75
alexis <as.alexismartinez@gmail.com>
2023-11-01 11:06:07 -0300

    feat: add /me

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,9 @@ import React from "react";
 import "./globals.css";
 import { Poppins } from "next/font/google";
 import Providers from "./state/providers";
+import { useEffect } from "react";
+import axios from "axios";
+import Cookies from "js-cookie";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -15,6 +18,19 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  useEffect(() => {
+    axios
+      .get("http://localhost:4000/api/v1/user/me", {
+        headers: {
+          cookies: `${Cookies.get("token")}`,
+        },
+      })
+      .then((response) => {
+        console.log("alexisss", response.data);
+      });
+    return () => {};
+  }, []);
+
   return (
     <html lang="en">
       <Providers>
