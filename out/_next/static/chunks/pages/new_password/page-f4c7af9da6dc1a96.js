22acbfb7f00599a3a067f4155abb40bbefc1d40a
alexis <as.alexismartinez@gmail.com>
2023-11-09 19:43:06 -0300

    add build

---
+++ out/_next/static/chunks/pages/new_password/page-f4c7af9da6dc1a96.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[15],{93:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/new_password/page",function(){return u(9059)}])}},function(n){n.O(0,[774,827,664,59,888,179],function(){return n(n.s=93)}),_N_E=n.O()}]);
